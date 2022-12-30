26ea27977396a6a566db692a374c4441c9838bf0
Tadas Kundelis <kundelist@gmail.com>
2022-12-31 00:40:44 +0200

    valid parentheses kata

---
+++ katas/valid-parentheses.hs
@@ -0,0 +1,13 @@
+module Codewars.Parentheses where
+-- https://www.codewars.com/kata/52774a314c2333f0a7000688/train/haskell
+
+validParentheses :: String -> Bool
+validParentheses s = parse s 0
+
+parse :: String -> Int -> Bool
+parse [] n = n == 0
+parse (x:xs) n
+  | n < 0 = False
+  | otherwise = case x of
+                  '(' -> parse xs (n + 1)
+                  ')' -> parse xs (n - 1)
